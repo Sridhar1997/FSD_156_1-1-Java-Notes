4c0e5d3cf867977d476496f955c4ccbbe9e151ff
Sridhar Raj P <45961122+Sridhar1997@users.noreply.github.com>
2025-10-07 15:29:21 +0530

    Add files via upload

---
+++ Day5P3.java
@@ -0,0 +1,17 @@
+public class Day5P3 {
+
+    static int sum(int k){
+        if(k>0){
+            return k+sum(k-1);
+        }
+        else{
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int answer=sum(10);
+        System.out.println(answer);
+    }
+    
+}
